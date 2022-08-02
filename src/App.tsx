1753530eb60334b46b799dbdfdb78a53bd7f3e5a
jgbpessoa <jgbpessoa@gmail.com>
2022-08-01 23:31:46 -0300

    Added Routes using react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import SearchWeather from "./pages/SearchWeather/SearchWeather"
+import DailyWeather from "./pages/DailyWeather/DailyWeather"
+import WeeklyWeather from "./pages/WeeklyWeather/WeeklyWeather"
 import "./style.css"
 
 function App() {
   return (
     <>
-      <SearchWeather />
+      <Router>
+        <Routes>
+          <Route path="/" element={<SearchWeather />} />
+          <Route path="/:city" element={<DailyWeather />} />
+          <Route path="/:city/weekly" element={<WeeklyWeather />} />
+        </Routes>
+      </Router>
     </>
   )
 }
